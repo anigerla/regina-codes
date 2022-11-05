b7d079bb41d80032711da330bb878ab00b1815cc
anigerla <regina.zaripova@gmail.com>
2022-11-05 08:23:19 -0400

    Reworked react components

---
+++ src/data.js
@@ -0,0 +1,64 @@
+const data = [
+    {
+        id: 1,
+        url: "https://www.arisgold.com/overview/default.aspx",
+        title: "Aris Gold",
+        img: "aris-gold.png"
+    },
+    {
+        id: 2,
+        url: "https://investors.intrepidpotash.com/home/default.aspx",
+        title: "Intrepid Potash",
+        img: "intrepid_potash.png"
+    },
+    {
+        id: 3,
+        url: "https://citiuspharma.com/home/default.aspx",
+        title: "Citius Pharma",
+        img: "citius-pharma.png"
+    },
+    {
+        id: 4,
+        url: "https://ir.vintagewineestates.com/overview/default.aspx",
+        title: "Vintage Wine Estates",
+        img: "vintage-wines.png"
+    },
+    {
+        id: 5,
+        url: "https://investors.evgo.com/resources/investor-faqs/default.aspx",
+        title: "EVgo",
+        img: "evgo.png" 
+    },
+    {
+        id: 6,
+        url: "https://investors.voltacharging.com/overview/default.aspx",
+        title: "Volta",
+        img: "volta.png"
+    },
+    {
+        id: 7,
+        url: "https://ir.envivabiomass.com/overview/default.aspx",
+        title: "Enviva",
+        img: "enviva.png"
+    },
+    {
+        id: 8,
+        url: "https://ir.ltcreit.com/overview/default.aspx",
+        title: "LTC Properties",
+        img: "logo.svg"
+    },
+    {
+        id: 9,
+        url: "https://www.invh.com/home/default.aspx",
+        title: "Invitation Homes",
+        img: "invitation_homes.png"
+    },
+    {
+        id: 10,
+        url: "https://investors.li-cycle.com/overview/default.aspx",
+        title: "Li-Cycle",
+        img: "licycle.png"
+    }
+]
+
+export default data
